2b538142f9f5488219a5cdcb0043405ad9628e45
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-24 17:54:05 -0300

    Adicionando as regras de negócio

--- Main.java
+++ Main.java
@@ -12,6 +12,25 @@ public class Main {
 		int intervaloFinal = sc.nextInt();
 		System.out.printf("Qual a quantidade de números que serão analisados? %n");
 		int qtdNumeros = sc.nextInt();
+		int numerosDentro = 0;
+		int numerosFora = 0;
+		int contadorDeInputs = 0;
+		
+		for (int X = 0; X < qtdNumeros; X++) {
+			contadorDeInputs++;
+			System.out.println("Digite o " + contadorDeInputs + "º número que será analisado:");
+			int numerosAnalisados = sc.nextInt();
+			if (numerosAnalisados >= intervaloInicial && numerosAnalisados <= intervaloFinal) {
+				numerosDentro += 1;
+			}else {
+				numerosFora += 1;
+			}
+		}
+		
+		System.out.println("O intervalo tem início no número: " + intervaloInicial + " e final no número: " + intervaloFinal);
+		System.out.println("A quantiade de números analisados foi de: " + qtdNumeros);
+		System.out.println("A quantidade de números dentro do intervalo é de: " + numerosDentro);
+		System.out.println("A quantidade de números fora do intervalo é de: " + numerosFora);
 
 	}
 
