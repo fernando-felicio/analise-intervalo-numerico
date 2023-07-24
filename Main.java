079637af0ced56c000e7aed0cf157442ff23bd53
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-24 17:23:35 -0300

    Adicionando funcionalidade de ponto inicial e ponto final do intervalo

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 package analise_intervalo_numerico;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("Digite um número inteiro que será o ponto inicial do intervalo: %n");
+		int intervaloInicial = sc.nextInt();
+		System.out.printf("Digite um número inteiro que será o ponto final do intervalo: %n");
+		int intervaloFinal = sc.nextInt();
+		System.out.printf("Qual a quantidade de números que serão analisados? %n");
+		int qtdNumeros = sc.nextInt();
 
 	}
 
