ec46332b6b89f6709a0801724dd0a1225cc5eb29
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-25 07:14:51 -0300

    Refatorando o código

--- Main.java
+++ Main.java
@@ -6,25 +6,31 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.printf("Digite um número inteiro que será o ponto inicial do intervalo: %n");
 		int intervaloInicial = sc.nextInt();
+		
 		System.out.printf("Digite um número inteiro que será o ponto final do intervalo: %n");
 		int intervaloFinal = sc.nextInt();
+		
 		System.out.printf("Qual a quantidade de números que serão analisados? %n");
 		int qtdNumeros = sc.nextInt();
+		
 		int numerosDentro = 0;
 		int numerosFora = 0;
-		int contadorDeInputs = 0;
 		
-		for (int X = 0; X < qtdNumeros; X++) {
-			contadorDeInputs++;
-			System.out.println("Digite o " + contadorDeInputs + "º número que será analisado:");
+		
+		for (int i = 0; i < qtdNumeros; i++) {
+			
+			System.out.println("Digite o " + i + "º número que será analisado:");
 			int numerosAnalisados = sc.nextInt();
 			if (numerosAnalisados >= intervaloInicial && numerosAnalisados <= intervaloFinal) {
 				numerosDentro += 1;
 			}else {
 				numerosFora += 1;
 			}
+			
+			sc.close();
 		}
 		
 		System.out.println("O intervalo tem início no número: " + intervaloInicial + " e final no número: " + intervaloFinal);
